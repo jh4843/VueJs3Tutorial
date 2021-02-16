91e5675d1449cfc2510a43b14a0f83fd60853b49
jh4843 <creamboy1@naver.com>
2021-02-16 23:15:11 +0900

    02_UserInput

---
+++ 02_HandlingUserInput/index.js
@@ -0,0 +1,14 @@
+const EventHandling = {
+    data() {
+      return {
+        message: 'Hello Vue.js!'
+      }
+    },
+    methods: {
+      reverseMessage() {
+        this.message = this.message.split('').reverse().join(' ')
+      }
+    }
+  }
+  
+  Vue.createApp(EventHandling).mount('#event-handling')
